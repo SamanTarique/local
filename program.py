e1bd054ef9d1ceec20488ad6d3a530666f6d9c8c
SamanTarique <samantarique02@gmail.com>
2025-02-04 10:25:52 +0500

    initial python file

---
+++ program.py
@@ -0,0 +1,27 @@
+'''import random
+gues=input("enter a number for guessing ")
+
+num=random.randrange(1,100)
+guess=int(gues)#casting
+if guess >num:
+    global message
+    message="guess is greater than num"
+    print(message)
+elif(guess <num):
+        print("guess is less than num")
+else:
+         print("program to war gia ")
+          
+print("chaar at index2= "+message[2])
+for x in message:
+    print(x)
+print("length os string "+len(message))
+print("slicing in string= "+ message[3:4])
+print("last char through negative silicing="+message[-5:-2])
+print("large" in message)# check large word is present in the message'''
+
+    
+
+   
+
+    
